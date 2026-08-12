4e4369363e2be94cf7c85ed19417c59af4336a9e
ozw <2575862318@qq.com>
2026-08-12 20:59:35 +0800

    feat: 一些基础配置

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,13 +6,13 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "assets/images/avatar.jpg",
 
 	// 名字
-	name: "Firefly",
+	name: "YunYuYue",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "风筝有风，海豚有海",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -22,21 +22,15 @@ export const profileConfig: ProfileConfig = {
 	// showName: true 时显示图标和名称，false 时只显示图标
 	links: [
 		{
-			name: "qq",
-			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
-			showName: false,
-		},
-		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/yunyuyue-web",
 			showName: false,
 		},
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "mailto:2575862318@qq.com",
 			showName: false,
 		},
 		{
--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,17 +6,17 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "YunYuYue",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "个人博客",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://blog.yunyuyue.xyz/",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"YunYuYue 的个人博客，分享技术、生活和学习的点滴。",
 
 	// 站点关键词
 	keywords: [
